e1cd94c4665c334e52a986beca044f2a8b0a3e33
max <max.assis@ymail.com>
2021-07-16 16:51:58 -0300

    consluida a primeira seção

--- src/components/Section1/index.jsx
+++ src/components/Section1/index.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Container } from "../container/index.jsx";
 import * as S from "./styles.jsx";
 import user from "../../assets/imgs/user.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import SwiperCore, { Pagination } from "swiper/core";
+import "./styles.css";
+SwiperCore.use([Pagination]);
 
 export default function Section1() {
   return (
     <S.ExternalContainer>
-      <Container>
+      <S.ContainerMain>
         <S.User>
           <h2>
             Olá <span>Usuário,</span> <br /> Bem-vindo ao portal da SEMA!
@@ -15,7 +21,44 @@ export default function Section1() {
         </S.User>
 
         <S.Curses>
-          <S.MyCurses></S.MyCurses>
+          <S.MyCurses>
+            <S.CurseTitle>
+              <h3>Meus cursos (2)</h3>
+            </S.CurseTitle>
+
+            <Swiper pagination={true} className="mySwiper">
+              <SwiperSlide>
+                <S.Slider>
+                  <S.Gradient>
+                    <S.CurseDescription>
+                      <div>
+                        <h3>Curso de plantação Caseira</h3>
+                      </div>
+                      <S.ButtonDiv>
+                        <h2>35% concluído</h2>
+                        <a href="#">Continuar</a>
+                      </S.ButtonDiv>
+                    </S.CurseDescription>
+                  </S.Gradient>
+                </S.Slider>
+              </SwiperSlide>
+              <SwiperSlide>
+                <S.Slider2>
+                  <S.Gradient>
+                    <S.CurseDescription>
+                      <div style={{ width: "90%" }}>
+                        <h3>Curso de apicultura</h3>
+                      </div>
+                      <S.ButtonDiv>
+                        <h2>85% concluído</h2>
+                        <a href="#">Continuar</a>
+                      </S.ButtonDiv>
+                    </S.CurseDescription>
+                  </S.Gradient>
+                </S.Slider2>
+              </SwiperSlide>
+            </Swiper>
+          </S.MyCurses>
 
           <S.Requests>
             <h3>Minhas solicitações (4)</h3>
@@ -47,7 +90,7 @@ export default function Section1() {
             </S.Status>
           </S.Requests>
         </S.Curses>
-      </Container>
+      </S.ContainerMain>
     </S.ExternalContainer>
   );
 }
--- src/components/Section1/styles.jsx
+++ src/components/Section1/styles.jsx
@@ -1,8 +1,15 @@
+import { Container } from "../container/index.jsx";
 import styled from "styled-components";
+import planta from "../../assets/imgs/planta2.png";
+import jardinagem from "../../assets/imgs/jardinagem2.png";
 
 export const ExternalContainer = styled.div`
   background-color: #f5f5f5;
   padding-top: 4.01875rem;
+`;
+
+export const ContainerMain = styled(Container)`
+  padding-bottom: 4.6875rem;
 `;
 
 export const User = styled.div`
@@ -47,13 +54,106 @@ export const MyCurses = styled.div`
   width: 100%;
   max-width: 18.0625rem;
   height: 20.5625rem;
-  border: 0.0625rem solid red;
+  opacity: 1;
+  background: #208e76;
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 8px;
+  opacity: 1;
+`;
+
+export const CurseTitle = styled.div`
+width:18.75rem;
+    height: 4.25rem;
+    padding-left: 0.75rem;
+    padding-top: 1.125rem;
+    padding-right: 6.375rem;
+    border-radius: 8px;
+
+    h3 {
+      text-align: left;
+      font: bold 1.5rem;
+      color: #ffffff;
+      opacity: 1;
+      font-size: 24px;
+    }
+  }
+`;
+
+export const SliderDiv = styled.div`
+  width: 100%;
+  height: 79.4%;
+`;
+
+export const Slider = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${planta});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const Gradient = styled.div`
+  background: transparent linear-gradient(180deg, #00000000 0%, #000000 100%) 0%
+    0% no-repeat padding-box;
+  height: 100%;
+  width: 100%;
+`;
+
+export const Slider2 = styled(Slider)`
+  background-image: url(${jardinagem});
+  background-repeat: no-repeat;
+`;
+
+export const CurseDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 8.5rem;
+  padding-left: 1rem;
+  padding-right: 1.0625rem;
+  padding-bottom: 2.75rem;
+
+  div {
+    width: "90%";
+    height: "2.75rem";
+  }
+
+  h3 {
+    text-align: left;
+    font: bold 1.1875rem;
+    color: #ffffff;
+    opacity: 1;
+  }
+`;
+
+export const ButtonDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 0.875rem;
+
+  h2 {
+    text-align: left;
+    font-size: 0.875rem;
+    font-weight: bold;
+    color: #ffffff;
+    opacity: 1;
+  }
+
+  a {
+    background: #f15a24 0% 0% no-repeat padding-box;
+    line-height: 2.1875rem;
+    padding: 0 23px;
+    text-align: center;
+    font: bold 14px;
+    color: #ffffff;
+    opacity: 1;
+    border-radius: 0.25rem;
+  }
 `;
 
 export const Requests = styled.div`
   width: 100%;
   max-width: 44rem;
-  border: 0.0625rem solid red;
   background-color: #ffffff;
   box-shadow: 0rem 0.1875rem 0.375rem #00000029;
   border-radius: 0.5rem;
@@ -67,6 +167,7 @@ export const Requests = styled.div`
     text-align: left;
     font: bold 16px;
     opacity: 1;
+    font-size: 24px;
   }
 
   p {
@@ -83,13 +184,12 @@ export const Status = styled.div`
   display: grid;
   grid-template-columns: 11rem 11rem 11rem;
   grid-column-gap: 2.778125rem;
-  border: 1px solid red;
 `;
 
 export const Item = styled.div`
   h2 {
     text-align: left;
-    font: bold 33px/24px;
+    font: bold 2.0625rem;
       color: #f15a24;
     }
   }
