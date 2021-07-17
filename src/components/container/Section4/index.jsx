ed2f045305365d02ec4ab49e36c6b4a8a17c852c
max <max.assis@ymail.com>
2021-07-17 19:46:36 -0300

    iniciando o footer

---
+++ src/components/container/Section4/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import * as S from "./styles.jsx";
+import face from "../../../assets/imgs/face.svg";
+
+export default function Section4() {
+  return (
+    <S.ExternalContainer>
+      <S.ContainerMain>
+        <S.Title>
+          <h3>Nossas redes sociais</h3>
+          <p>
+            Não perca mais nenhuma novidade. Siga a SEMA nas rede sociais e
+            fique por dentro de tudo o que acontece
+          </p>
+        </S.Title>
+        <img src={face} alt=" face icon" />
+      </S.ContainerMain>
+    </S.ExternalContainer>
+  );
+}
---
+++ src/components/container/Section4/styles.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Container } from "../index.jsx";
+
+export const ExternalContainer = styled.div`
+  background-color: #f5f5f5;
+`;
+
+export const ContainerMain = styled(Container)`
+  padding-top: 2.75rem;
+  padding-bottom: 2.625rem;
+  background-color: #f5f5f5;
+`;
+
+export const Title = styled.div`
+  h3 {
+    text-align: left;
+    font: 2rem;
+    font-weight: bold;
+    color: #f15a24;
+  }
+
+  p {
+    text-align: left;
+    font: 1.6875rem;
+    color: #585858;
+    margin-bottom: 2.4456rem;
+  }
+`;
